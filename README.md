2e76e140384d8e1c74b9b300042ce9d56de9918d
Oli Evans <oli@tableflip.io>
2021-05-07 09:57:02 +0100

    📡✨⬢
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ README.md
@@ -0,0 +1,31 @@
+# ipfs-get 📡✨⬢
+
+> Get and verify a file by CID from an IPFS gateway over http.
+
+```console
+$ ipfs-get bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy
+📡 Fetching .car file from https://ipfs.io
+🔐 Verified 3/3 blocks
+✅ Wrote ./bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy
+```
+
+A thin wrapper over [@ipld/car]() and [unix-fs-exporter](https://github.com/ipfs/js-ipfs-unixfs/tree/master/packages/ipfs-unixfs-exporter). It fetches the data by from the IPFS gateway api as a .car file, extacts the cids and blocks, verifying them as it goes, and writes the reassembled data to disk.
+
+⚠️ **This is a proof-of-concept and should not be consumed without consulting your doctor first.**
+
+## How
+
+- Fetch a .car file for the root CID from `/api/v0/dag/export`.
+  - this could fail if the full dag is unavailable. it's ok.
+- Index the cid and block postions in the car file
+- Start at the root as defined by the car file header
+  - ? verify it matches the cid the user provided
+- verify each block by rehashing the data as you pull it from the car as per https://github.com/ipld/js-car/blob/master/examples/verify-car.js
+- re-assemble the blocks into the thing and write it to disk.
+
+## Limitations
+
+Lot's! But only while we get a first pass working.
+
+- only dag-pb unixfs for now. cbor/json next.
+- node / cli only. browser next.
---
+++ index.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+import fs from 'fs'
+import path from 'path'
+import meow from 'meow'
+import fetch from 'isomorphic-unfetch'
+import { bytes, CID } from 'multiformats'
+import * as dagPb from '@ipld/dag-pb'
+import * as raw from 'multiformats/codecs/raw'
+import { sha256 } from 'multiformats/hashes/sha2'
+import { CarReader } from '@ipld/car'
+import exporter from 'ipfs-unixfs-exporter'
+import toIterable from 'stream-to-it'
+import { pipe } from 'it-pipe'
+import { map } from 'streaming-iterables'
+
+const { toHex } = bytes
+
+const codecs = {
+  [dagPb.code]: dagPb,
+  [raw.code]: raw
+}
+
+const hashes = {
+  [sha256.code]: sha256
+}
+
+const options = {
+  importMeta: import.meta,
+  flags: {
+    gateway: {
+      type: 'string',
+      alias: 'g',
+      default: 'http://127.0.0.1:5001'
+    }
+  }
+}
+
+const cli = meow(`
+  Usage
+  $ ipfs-get <cid>
+`, options)
+
+ipfsGet({cid: cli.input[0], gateway: new URL(cli.flags.gateway)})
+
+async function ipfsGet ({cid, gateway}) {
+  console.log(`📡 Fetching .car file from ${gateway}`)
+  const carStream = await fetchCar(cid, gateway)
+  const carReader = await CarReader.fromIterable(carStream)
+
+  let count = 0
+  const verifyingIpldAdaptor = {
+    get: async (cid) => {
+      const res = await carReader.get(cid)
+      if (!isValid(res)) {
+        throw new Error(`Bad block. Hash does not match CID ${cid}`)
+      }
+      const obj = decode(res)
+      count++
+      return obj
+    }
+  }
+
+  await extractCar({cid, ipld: verifyingIpldAdaptor, output: process.cwd()})
+  console.log(`🔐 Verified ${count}/${count} block${count === 1 ? '': 's'}`)
+  console.log(`✅ Wrote ${cid}`)
+}
+
+async function extractCar ({cid, ipld, output}) {
+  // magic extracted from js-ipfs:
+  // https://github.com/ipfs/js-ipfs/blob/46618c795bf5363ba3186645640fb81349231db7/packages/ipfs-core/src/components/get.js#L20
+  // https://github.com/ipfs/js-ipfs/blob/46618c795bf5363ba3186645640fb81349231db7/packages/ipfs-cli/src/commands/get.js#L56-L66
+  for await (const file of exporter.recursive(cid, ipld, { /* options */ })) {
+    const fullFilePath = path.join(output, file.path)
+    if (file.type === 'file' || file.type === 'raw') {
+      await fs.promises.mkdir(path.join(output, path.dirname(file.path)), { recursive: true })
+      await pipe(
+        file.content,
+        toIterable.sink(fs.createWriteStream(fullFilePath))
+      )
+    } else {
+      // this is a dir
+      await fs.promises.mkdir(fullFilePath, { recursive: true })
+    }
+  } 
+}
+
+async function fetchCar (cid, gateway) {
+  const url = `${gateway}api/v0/dag/export?arg=${cid}`
+  const res = await fetch(url, { method: 'POST'})
+  if (res.status > 400) {
+    throw new Error(`${res.status} ${res.statusText} ${url}`)
+  }
+  return res.body
+}
+
+async function isValid ({cid, bytes}) {
+  const hashfn = hashes[cid.multihash.code]
+  if (!hashfn) {
+    throw new Error(`Missing hash function for ${cid.multihash.code}`)
+  }
+  const hash = await hashfn.digest(bytes)
+  return toHex(hash.digest) === toHex(cid.multihash.digest)
+}
+
+async function decode ({cid, bytes}) {
+  const codec = codecs[cid.code]
+  if (!codec) {
+    throw new Error(`Missing codec for ${cid.code}`)
+  }
+  return codec.decode(bytes)
+}
