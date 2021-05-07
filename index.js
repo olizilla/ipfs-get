8a7ea8d1a536198fdfab8b6c8c81208a698e8012
Oli Evans <oli@tableflip.io>
2021-05-07 12:32:41 +0100

    feat: add --output flag
    let the user tell us what they want to call it. Apparently some do not like reading CIDs.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 import fs from 'fs'
-import path from 'path'
 import meow from 'meow'
 import fetch from 'isomorphic-unfetch'
-import { bytes, CID } from 'multiformats'
+import { bytes } from 'multiformats'
 import * as dagPb from '@ipld/dag-pb'
 import * as raw from 'multiformats/codecs/raw'
 import { sha256 } from 'multiformats/hashes/sha2'
@@ -11,7 +10,6 @@ import { CarReader } from '@ipld/car'
 import exporter from 'ipfs-unixfs-exporter'
 import toIterable from 'stream-to-it'
 import { pipe } from 'it-pipe'
-import { map } from 'streaming-iterables'
 
 const { toHex } = bytes
 
@@ -31,6 +29,10 @@ const options = {
       type: 'string',
       alias: 'g',
       default: 'http://127.0.0.1:5001'
+    },
+    output: {
+      type: 'string',
+      alias: 'o'
     }
   }
 }
@@ -40,9 +42,13 @@ const cli = meow(`
   $ ipfs-get <cid>
 `, options)
 
-ipfsGet({cid: cli.input[0], gateway: new URL(cli.flags.gateway)})
+ipfsGet({
+  cid: cli.input[0],
+  gateway: new URL(cli.flags.gateway),
+  output: cli.flags.output
+})
 
-async function ipfsGet ({cid, gateway}) {
+async function ipfsGet ({ cid, gateway, output }) {
   console.log(`📡 Fetching .car file from ${gateway}`)
   const carStream = await fetchCar(cid, gateway)
   const carReader = await CarReader.fromIterable(carStream)
@@ -60,40 +66,42 @@ async function ipfsGet ({cid, gateway}) {
     }
   }
 
-  await extractCar({cid, ipld: verifyingIpldAdaptor, output: process.cwd()})
-  console.log(`🔐 Verified ${count}/${count} block${count === 1 ? '': 's'}`)
-  console.log(`✅ Wrote ${cid}`)
+  await extractCar({ cid, ipld: verifyingIpldAdaptor, output })
+  console.log(`🔐 Verified ${count}/${count} block${count === 1 ? '' : 's'}`)
+  console.log(`✅ Wrote ${output || cid}`)
 }
 
-async function extractCar ({cid, ipld, output}) {
+async function extractCar ({ cid, ipld, output }) {
+  const depth = 0
   // magic extracted from js-ipfs:
   // https://github.com/ipfs/js-ipfs/blob/46618c795bf5363ba3186645640fb81349231db7/packages/ipfs-core/src/components/get.js#L20
   // https://github.com/ipfs/js-ipfs/blob/46618c795bf5363ba3186645640fb81349231db7/packages/ipfs-cli/src/commands/get.js#L56-L66
   for await (const file of exporter.recursive(cid, ipld, { /* options */ })) {
-    const fullFilePath = path.join(output, file.path)
+    // output overides the first part of the path.
+    const filePath = depth === 0 ? output || file.path : file.path
     if (file.type === 'file' || file.type === 'raw') {
-      await fs.promises.mkdir(path.join(output, path.dirname(file.path)), { recursive: true })
+      // await fs.promises.mkdir(fullFilePath), { recursive: true })
       await pipe(
         file.content,
-        toIterable.sink(fs.createWriteStream(fullFilePath))
+        toIterable.sink(fs.createWriteStream(filePath))
       )
     } else {
       // this is a dir
-      await fs.promises.mkdir(fullFilePath, { recursive: true })
+      await fs.promises.mkdir(filePath, { recursive: true })
     }
-  } 
+  }
 }
 
 async function fetchCar (cid, gateway) {
   const url = `${gateway}api/v0/dag/export?arg=${cid}`
-  const res = await fetch(url, { method: 'POST'})
+  const res = await fetch(url, { method: 'POST' })
   if (res.status > 400) {
     throw new Error(`${res.status} ${res.statusText} ${url}`)
   }
   return res.body
 }
 
-async function isValid ({cid, bytes}) {
+async function isValid ({ cid, bytes }) {
   const hashfn = hashes[cid.multihash.code]
   if (!hashfn) {
     throw new Error(`Missing hash function for ${cid.multihash.code}`)
@@ -102,7 +110,7 @@ async function isValid ({cid, bytes}) {
   return toHex(hash.digest) === toHex(cid.multihash.digest)
 }
 
-async function decode ({cid, bytes}) {
+async function decode ({ cid, bytes }) {
   const codec = codecs[cid.code]
   if (!codec) {
     throw new Error(`Missing codec for ${cid.code}`)
