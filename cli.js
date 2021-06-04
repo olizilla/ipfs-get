07201959338e9a2c47d031215263107a751de3f6
Oli Evans <oli.evans@gmail.com>
2021-06-04 11:05:35 +0100

    feat: use ipfs.io as default gatway (#10)
    - ipfs.io now has /api/v0/dag/export powers!
    
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- cli.js
+++ cli.js
@@ -8,7 +8,7 @@ const options = {
     gateway: {
       type: 'string',
       alias: 'g',
-      default: 'http://127.0.0.1:5001'
+      default: 'https://ipfs.io'
     },
     output: {
       type: 'string',
@@ -18,12 +18,21 @@ const options = {
 }
 
 const cli = meow(`
-  Usage
-  $ ipfs-get <cid>
+Usage
+  $ ipfs-get <cid> [--gateway https://ipfs.io] [--output <path>]
+Example
+  $ ipfs-get bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy --output guardian.jpg
+Options
+  --gateway <URL> the ipfs gateway to fetch the cid from as a CAR.
+  --output <path> where to write the output to
 `, options)
+
+if (!cli.input[0]) {
+  cli.showHelp()
+}
 
 ipfsGet({
   cid: cli.input[0],
-  gateway: new URL(cli.flags.gateway),
+  gateway: cli.flags.gateway,
   output: cli.flags.output
 })
